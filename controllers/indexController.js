cfb1dd83f777cbfe5737c66d132ed8ab8931f840
FernandaElola <elolafernanda@gmail.com>
2021-09-05 12:03:28 -0300

    correccion bonus track

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 let products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
-let about = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','about.json'),'utf-8'));
 
 module.exports = {
     index : (req,res) => {
         return res.render('index', {
-        title : "Pimienta&Sal",
-        products,
-        about 
+        title : "Pimienta & Sal",
+        imageAbout : "principal.jpg",
+        products : JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8')),
     });
 },
     detail : (req,res) => {
